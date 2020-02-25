08e002eb21e31e7cc46b3669b5f749718e60e825
Abdo Tareq <abdotarekfathy@gmail.com>
2020-02-25 20:50:38 +0200

    First commit

---
+++ app/src/main/java/com/abdotareq/subwaye_ticketting/MainActivity.java
@@ -0,0 +1,16 @@
+package com.abdotareq.subwaye_ticketting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
