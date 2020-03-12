4cc175f0f61a0b3abb1b1aab7d9fcf599cace7e5
Abdo Tareq <abdotarekfathy@gmail.com>
2020-03-12 19:06:32 +0200

    Add ViewBinding

--- app/src/main/java/com/abdotareq/subwaye_ticketting/MainActivity.java
+++ app/src/main/java/com/abdotareq/subwaye_ticketting/MainActivity.java
@@ -3,13 +3,32 @@ package com.abdotareq.subwaye_ticketting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.abdotareq.subwaye_ticketting.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
 
     }
