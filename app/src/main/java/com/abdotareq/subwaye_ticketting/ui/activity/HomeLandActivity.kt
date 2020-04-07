adefee9c774b5b0fca41e99693d25fb0d669ea2e
Abdo Tareq <abdotarekfathy@gmail.com>
2020-04-07 21:36:26 +0200

    Add big center icon and animate bottom nav
    added an ImageView over the navigation view and positioned it center so that 3rd default menu option get hidden and the add ImageView button cover the center position and ultimately we get the final result like that

--- app/src/main/java/com/abdotareq/subwaye_ticketting/ui/activity/HomeLandActivity.kt
+++ app/src/main/java/com/abdotareq/subwaye_ticketting/ui/activity/HomeLandActivity.kt
@@ -2,7 +2,10 @@ package com.abdotareq.subwaye_ticketting.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.abdotareq.subwaye_ticketting.R
+import kotlinx.android.synthetic.main.activity_home_land.*
 
 class HomeLandActivity : AppCompatActivity() {
 
@@ -10,6 +13,10 @@ class HomeLandActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_land)
 
+        // this for icon background tint to be empty(just lines make image)
+        bottom_navigation_main.itemIconTintList = null
+
 
     }
+
 }
