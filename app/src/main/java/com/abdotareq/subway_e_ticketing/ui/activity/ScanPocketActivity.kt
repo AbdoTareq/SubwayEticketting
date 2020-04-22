3afd49bd1153227495678d5ce2aaa7f17221c977
Abdo Tareq <abdotarekfathy@gmail.com>
2020-04-22 07:44:20 +0200

    Finish generation QR ticket Screen

--- app/src/main/java/com/abdotareq/subway_e_ticketing/ui/activity/ScanPocketActivity.kt
+++ app/src/main/java/com/abdotareq/subway_e_ticketing/ui/activity/ScanPocketActivity.kt
@@ -1,13 +1,64 @@
 package com.abdotareq.subway_e_ticketing.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.appcompat.app.AppCompatActivity
 import com.abdotareq.subway_e_ticketing.R
+import com.abdotareq.subway_e_ticketing.databinding.ActivityScanPocketBinding
+import com.abdotareq.subway_e_ticketing.model.BoughtTicket
+import com.abdotareq.subway_e_ticketing.model.InTicket
+import timber.log.Timber
+
 
 class ScanPocketActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityScanPocketBinding
+
+    private var num: Int = 0
+
+    private var checkInTicket = InTicket()
+    private var boughtTicket = BoughtTicket()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_pocket)
+        binding = ActivityScanPocketBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // get ticket
+        try {
+            num = 1
+            checkInTicket = intent?.getParcelableExtra("checkInTicket") as InTicket
+            Timber.e("$checkInTicket")
+            binding.title.text = getString(R.string.check_out)
+            binding.price.text = String.format(getString(R.string.ticket_price_format, checkInTicket.price))
+            binding.instructions.text = String.format(getString(R.string.scan_mess_format, getString(R.string.exit), getString(R.string.check_out)))
+
+
+        } catch (e: Exception) {
+            Timber.e(e)
+            num = 2
+            boughtTicket = intent?.getParcelableExtra("boughtTicket") as BoughtTicket
+            binding.title.text = getString(R.string.check_in)
+            binding.price.text = String.format(getString(R.string.ticket_price_format, boughtTicket.price))
+            binding.instructions.text = String.format(getString(R.string.scan_mess_format, getString(R.string.entrance), getString(R.string.use)))
+        }
+
+
+        // Initializing the QR Encoder with your value to be encoded, type you required and Dimension
+        val qrgEncoder = if (num == 1)
+            QRGEncoder(checkInTicket.id, null, QRGContents.Type.TEXT, 650)
+        else
+            QRGEncoder(boughtTicket.id, null, QRGContents.Type.TEXT, 650)
+
+        try {
+            // Getting QR-Code as Bitmap
+            val bitmap = qrgEncoder.bitmap
+            // Setting Bitmap to ImageView
+            binding.qrImage.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
     }
 }
