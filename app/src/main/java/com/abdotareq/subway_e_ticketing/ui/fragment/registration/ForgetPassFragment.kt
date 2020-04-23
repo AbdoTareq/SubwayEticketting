14c351e5fd3e12ffdfd2da4c178195f52db8049f
Abdo Tareq <abdotarekfathy@gmail.com>
2020-04-23 19:00:47 +0200

    Change ForgetPassFragment to use vewModel

--- app/src/main/java/com/abdotareq/subway_e_ticketing/ui/fragment/registration/ForgetPassFragment.kt
+++ app/src/main/java/com/abdotareq/subway_e_ticketing/ui/fragment/registration/ForgetPassFragment.kt
@@ -6,19 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.abdotareq.subway_e_ticketing.R
 import com.abdotareq.subway_e_ticketing.databinding.FragmentForgetPassBinding
-import com.abdotareq.subway_e_ticketing.model.ErrorStatus
 import com.abdotareq.subway_e_ticketing.model.ErrorStatus.Codes.getErrorMessage
-import com.abdotareq.subway_e_ticketing.model.User
-import com.abdotareq.subway_e_ticketing.network.UserApiObj
+import com.abdotareq.subway_e_ticketing.model.UserInterface
 import com.abdotareq.subway_e_ticketing.utility.util
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import timber.log.Timber
+import com.abdotareq.subway_e_ticketing.viewmodels.ForgetPassViewModel
 
 /**
  *  [ForgetPassFragment] responsible for sending verification code to user mail
@@ -32,54 +28,58 @@ class ForgetPassFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: ForgetPassViewModel
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentForgetPassBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.forgetPassSendVerificationBtn.setOnClickListener {
-            if (!util.isValidEmail(binding.forgetPassMailEt.text.toString()))
+        val application = requireNotNull(activity).application
+
+        viewModel = ViewModelProvider(this).get(ForgetPassViewModel::class.java)
+
+        binding.viewModel = viewModel
+        // Specify the current activity as the lifecycle owner of the binding. This is used so that
+        // the binding can observe LiveData updates
+        binding.lifecycleOwner = this
+
+        viewModel.eventSendCode.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                viewModel.onSendCodeComplete()
                 sendVerificationCode(binding.forgetPassMailEt.text.toString())
-            else {
-                binding.forgetPassMailEt.error = getString(R.string.invalid_mail_warning)
-                binding.forgetPassMailEt.requestFocus()
             }
-        }
+        })
 
         return view
     }
 
     // send send Verification Code to user mail
     private fun sendVerificationCode(mail: String) {
-        val user = User()
-        user.email = mail
         //initialize and show a progress dialog to the user
         val progressDialog = util.initProgress(context, getString(R.string.loading))
-        progressDialog.show()
 
-        //start the call
-        UserApiObj.retrofitService.sendVerificationCodeCall(user)?.enqueue(object : Callback<ResponseBody?> {
-            override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
-                val responseCode = response.code()
-                if (responseCode in 200..299 && response.body() != null) {
-                    //user saved successfully
-                    progressDialog.dismiss()
-                    // this how to navigate between fragments using safe args after defining action in navigation xml file
-                    // between desired fragments
-                    findNavController().navigate(ForgetPassFragmentDirections.actionForgetPassFragmentToVerificationFragment(mail))
-                } else {
-                    //user not saved successfully
-                    progressDialog.dismiss()
-                    Toast.makeText(context, getErrorMessage(responseCode, activity!!.application), Toast.LENGTH_LONG).show()
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
+        val userInterface: UserInterface = object : UserInterface {
+            override fun onSuccess() {
                 progressDialog.dismiss()
-                Toast.makeText(context, getErrorMessage(ErrorStatus.Codes.NoNetworkException, activity!!.application), Toast.LENGTH_LONG).show()
-                //                //ToDo: Remove Toast
-                Timber.e("FAILED : ${t.message}")
+                // this how to navigate between fragments using safe args after defining action in navigation xml file
+                // between desired fragments
+                findNavController().navigate(ForgetPassFragmentDirections.actionForgetPassFragmentToVerificationFragment(mail))
             }
-        })
+
+            override fun onFail(responseCode: Int) {
+                progressDialog.dismiss()
+                Toast.makeText(context, getErrorMessage(responseCode, activity!!.application), Toast.LENGTH_LONG).show()
+            }
+        }
+        //start the call
+        if (viewModel.validateMail()) {
+            progressDialog.show()
+            viewModel.forgetPass(userInterface)
+        } else {
+            binding.mailTi.error = getString(R.string.invalid_mail_warning)
+            binding.mailTi.requestFocus()
+        }
     }
 
     override fun onDestroyView() {
