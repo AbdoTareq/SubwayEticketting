8e97db15a7130a8f9777a1a39d32661ddc20dbdd
Abdo Tareq <abdotarekfathy@gmail.com>
2020-05-02 22:58:22 +0200

    refactor: Settings to be in the same place

---
+++ app/src/main/java/com/abdotareq/subway_e_ticketing/ui/fragment/AppSettingsFragment.kt
@@ -0,0 +1,38 @@
+package com.abdotareq.subway_e_ticketing.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.abdotareq.subway_e_ticketing.databinding.FragmentAppSettingsBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AppSettingsFragment : Fragment() {
+    private var _binding: FragmentAppSettingsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAppSettingsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.profile.setOnClickListener {
+            findNavController().navigate(AppSettingsFragmentDirections.actionAppSettingsFragmentToProfileSettingsFragment())
+        }
+
+        binding.appSettings.setOnClickListener {
+            findNavController().navigate(AppSettingsFragmentDirections.actionAppSettingsFragmentToSettingsFragment())
+        }
+
+
+
+        return view
+    }
+
+}
